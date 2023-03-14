057f677720bd9856723a787b84f76aa0cadb3813
NachoMol <ignaciotmolina@gmail.com>
2023-03-13 22:56:02 -0300

    subiendo ejercicio clase 11S

--- repos-clase/semana-4/clase-11.js
+++ repos-clase/semana-4/clase-11.js
@@ -1,14 +1,71 @@
 /* -------------------------------------------------------------------------- */
 /*               [1] FUNCION: capturar los datos del formulario               */
 /* -------------------------------------------------------------------------- */
-function capturarDatosFormulario() {}
+function capturarDatosFormulario() {
+  // 👇 establecemos un objeto vacío para despues rellenarlo
+  const objetoInformacion = {
+    nombre: "",
+    password: "",
+    telefono: "",
+    hobbies: [],
+    nacionalidad: "",
+  };
+
+  // capturamos todos los nodos
+  const nom = document.getElementById("nom");
+  const pass = document.querySelector("#pass");
+  const tel = document.querySelector("#tel");
+  const hobbies = document.querySelectorAll("[name=hobbies]");
+  const nacionalidad = document.querySelectorAll("[name=nacionalidad]");
+  // const nacionalidad = document.querySelectorAll("[type=radio]");
+  console.log(hobbies);
+
+  // 👇 rellenamos el objeto con la info correspondiente
+  objetoInformacion.nombre = nom.value;
+  objetoInformacion.password = pass.value;
+  objetoInformacion.telefono = tel.value;
+  // recorremos los checkbox
+  hobbies.forEach((hobbie) => {
+    // Para cada hobbie seleccionado lo sumamos al array de hobbies
+    if (hobbie.checked) {
+      objetoInformacion.hobbies.push(hobbie.id);
+    }
+  });
+  // recorremos los nacionalidad
+  nacionalidad.forEach((nacion) => {
+    // Para cada hobbie seleccionado lo sumamos al array de hobbies
+    if (nacion.checked) {
+      objetoInformacion.nacionalidad = nacion.id;
+    }
+  });
+
+  return objetoInformacion;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                 [2] FUNCION: escuchamos el submit del form                 */
 /* -------------------------------------------------------------------------- */
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function (evento) {});
+form.addEventListener("submit", function (evento) {
+  // prevenimos el default para que no se intente enviar
+  evento.preventDefault();
+
+  // utilizamos nuestra funcion para capturar los datos
+  const datos = capturarDatosFormulario();
+  console.log(datos);
+
+  const errores = validarInformacion(datos);
+  console.log(errores);
+
+  // mostramos los errores presentes
+  renderizarErrores(errores);
+
+  // mostramos mensaje de exito si no hay errores
+  mostrarMensajeExito(errores);
+
+  form.reset();
+});
 
 /* ----------------------------- MESA DE TRABAJO ---------------------------- */
 /* -------------------------------------------------------------------------- */
@@ -23,10 +80,27 @@ form.addEventListener("submit", function (evento) {});
 // 4- Si el telefono tiene menos de 10 números, sumar el error: "No es un teléfono válido."
 // 5- Si la lista de hobbies tiene más de 4 items, sumar el error: "Sólo es posible seleccionar 4 hobbies."
 // 5- Si no hay una nacionalidad definida, sumar el error: "Debe seleccionar una nacionalidad."
-function validarInformacion(usuario) {
-  let errores = [];
+function validarInformacion(datos) {
+
+  let errores = []
+
   // 👇 desarrollar aqui la funcion
 
+if(datos.nombre.length < 3 || !isNaN(datos.nombre)){
+  errores.push("El nombre debe tener al menos 3 caracteres.")
+}
+if(datos.password.trim().length < 6){
+  errores.push("La contraseña debe tener al menos 6 caracteres, entre letras y símbolos.")
+}
+if(datos.telefono.length < 10){
+  errores.push("No es un teléfono válido")
+}
+if(datos.hobbies.length > 4){
+  errores.push("Sólo es posible seleccionar 4 hobbies.")
+}
+if(datos.nacionalidad == null){
+  errores.push("Debe seleccionar una nacionalidad.")
+}
   return errores;
 }
 
