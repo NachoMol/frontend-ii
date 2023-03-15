19746aa964bea3fd4b1f4be5384b16f67e8c8a90
NachoMol <ignaciotmolina@gmail.com>
2023-03-14 21:42:27 -0300

    actualizacion parcial

--- Primer entregable FrontEnd-ii/consignas 3.0 - ESTUDIANTE/code.js
+++ Primer entregable FrontEnd-ii/consignas 3.0 - ESTUDIANTE/code.js
@@ -39,10 +39,15 @@ function obtenerDatosDelUsuario() {
   datosPersona.nombre = prompt("Ingrese su nombre: ")
   datosPersona.edad = 2023 - prompt("Ingrese su año de nacimiento: ")
   datosPersona.ciudad = prompt("Ingrese su ciudad de origen: ")
-  datosPersona.interesPorJs = prompt("Tiene interés por JS? indique true / false")
+  datosPersona.interesPorJs = confirm("Tiene interés por JS?")
+
+  if(datosPersona.interesPorJs){
+    datosPersona.interesPorJs = "Si"
+  }else{
+    datosPersona.interesPorJs = "No"
+  }
 
 }
-
 
 function renderizarDatosUsuario() {
   /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
@@ -54,69 +59,50 @@ function renderizarDatosUsuario() {
   let indexCiudad = document.querySelector("#ciudad")
   let indexJS = document.querySelector("#javascript")
 
-  indexNombre.innerHTML += datosPersona.nombre
-  indexEdad.innerHTML += datosPersona.edad
-  indexCiudad.innerHTML += datosPersona.ciudad
-  indexJS.innerHTML += datosPersona.interesPorJs
+  indexNombre.innerHTML = datosPersona.nombre
+  indexEdad.innerHTML = datosPersona.edad
+  indexCiudad.innerHTML = datosPersona.ciudad
+  indexJS.innerHTML = datosPersona.interesPorJs
 
 }
 
-renderizarDatosUsuario();
-
-function recorrerListadoYRenderizarTarjetas() {
   /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
-  
-
-  let botonMaterias = document.getElementById("obtener-materias")
   let fila = document.getElementById("fila")
-
-  botonMaterias.addEventListener('click', function(){
-
+  function recorrerListadoYRenderizarTarjetas() {
+  
     listado.forEach(item => {
+
       fila.innerHTML += `
       
       <article class="caja">
       
         <image src= "${item.imgUrl}" alt= "${item.lenguajes}"></image>
-        <p class="lenguajes">"${item.lenguajes}"</p>
-        <p class="bimestre">"${item.bimestre}"</p>
-
+        <p class="lenguajes"> Lenguaje: ${item.lenguajes}</p>
+        <p class="bimestre"> Bimestre: ${item.bimestre}</p>
       </article>
       `
     
-    });
+      }
+    )
 
-    botonMaterias.removeEventListener('click',function(){
-
-      listado.forEach(item => {
-        fila.innerHTML += `
-        
-        <article class="caja">
-        
-          <image src= "${item.imgUrl}" alt= "${item.lenguajes}"></image>
-          <p class="lenguajes">"${item.lenguajes}"</p>
-          <p class="bimestre">"${item.bimestre}"</p>
-  
-        </article>
-        `
-      
-      });
-
-    })
-
-  })
-
+    materiasBtn.removeEventListener("click", recorrerListadoYRenderizarTarjetas)
 }
-
-recorrerListadoYRenderizarTarjetas()
 
 function alternarColorTema() {
   /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
- 
+  let sitio = document.getElementById("sitio")
   
-
+  sitio.classList.toggle("dark")
 
 }
 
 /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
 
+let textoOculto = document.getElementById("sobre-mi")
+
+document.addEventListener("keydown", function(e){
+
+  if(e.key == "f"){
+    textoOculto.classList.remove("oculto")
+  }
+})
