fbe777d78c23988b0136339e7d8899cae8913207
NachoMol <ignaciotmolina@gmail.com>
2023-03-27 21:34:48 -0300

    Subiendo semana 6

---
+++ repos-clase/semana-6/js/clase-18.js
@@ -0,0 +1,51 @@
+/* -------------------------------------------------------------------------- */
+/*                [1] FUNCION: esperamos la carga de la ventana               */
+/* -------------------------------------------------------------------------- */
+window.addEventListener('load', function () {
+
+
+})
+
+/* -------------------------------------------------------------------------- */
+/*                 [2] FUNCION: capturamos los datos del form                 */
+/* -------------------------------------------------------------------------- */
+function capturarDatos() {
+
+   
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                    [3] FUNCION: enviar(postear) a la API                   */
+/* -------------------------------------------------------------------------- */
+// Nos basamos en la documentacion de la API:
+// https://jsonplaceholder.typicode.com/guide/
+
+function postearComentario() {
+    // 👇 usamos nuestra funcion para capturar los datos y guardarlos como objeto
+    
+     
+
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                      [4] FUNCION: renderizar respuesta                     */
+/* -------------------------------------------------------------------------- */
+
+function renderizarRespuesta(datos) {
+
+
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                               MESA DE TRABAJO                              */
+/* -------------------------------------------------------------------------- */
+// Mesa de Trabajo Clase 18
+// To-Do API
+// Valiéndose de la documentación y el trabajo en clase, deberán lograr crear un usuario en la API y obtener su token.
+// Este proceso puede realizarse a través de la herramienta Postman, o si ya lo prefieren desde un script.
+// Documentación: https://todo-api.ctd.academy/#/tasks/createTask
+// url: https://todo-api.ctd.academy/v1/
+// Path para crear usuario users
