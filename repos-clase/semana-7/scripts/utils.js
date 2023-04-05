3716a6078d9e36028b33030efe30008edfbfb557
NachoMol <ignaciotmolina@gmail.com>
2023-04-04 22:23:15 -0300

    subiendo utils

--- repos-clase/semana-7/scripts/utils.js
+++ repos-clase/semana-7/scripts/utils.js
@@ -4,16 +4,18 @@ function validarTexto(texto) {
 }
 
 function normalizarTexto(texto) {
-    
+    firstName = firstName.toLowerCase()
+    lastName = lastName.toLowerCase()
 }
 
 /* ---------------------------------- email --------------------------------- */
 function validarEmail(email) {
-    
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return regex.test(email)
 }
 
 function normalizarEmail(email) {
-    
+    return email.toLowerCase()
 }
 
 /* -------------------------------- password -------------------------------- */
